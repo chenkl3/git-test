8f84aec84f314145d2feec7fd6e16491d0534592
admin <2283516135@qq.com>
2020-02-13 08:33:50 +0800

    ces

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("hello world!")
+count = 0
+for i in (1,2,3,4):
+    for j in (1,2,3,4):
+        for k in (1,2,3,4):
+            if i !=j and i !=j and j != k:
+                count += 1
+print(count)
