1fa090a9f36f74d38abef0f1e96a2d8fad2b477f
chenkl3 <2283516135@qq.com>
2020-03-04 10:24:47 +0800

    ces

--- test.py
+++ test.py
@@ -75,7 +75,7 @@ class DownPic(threading.Thread):
                     os.makedirs(path)
                     print()
                 else:
-                    print("77790")
+                    print("7779099")
                 for pic in values:
                     filename = path +"/"+pic.split('/')[-1]
                     if os.path.exists(filename):
